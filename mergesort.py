9276c7575504885c5ad09c5b082f6154b45b1478
mokshithamonala <195851781+mokshithamonala@users.noreply.github.com>
2026-01-09 12:12:47 +0530

    Add files via upload

---
+++ mergesort.py
@@ -0,0 +1,55 @@
+def merge(arr, left, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
+
+    # Create temp arrays
+    L = [0] * n1
+    R = [0] * n2
+
+    # Copy data to temp arrays
+    for i in range(n1):
+        L[i] = arr[left + i]
+    for j in range(n2):
+        R[j] = arr[mid + 1 + j]
+
+    i = 0
+    j = 0
+    k = left
+
+    # Merge the temp arrays back into arr[left..right]
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    # Copy remaining elements of L[], if any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    # Copy remaining elements of R[], if any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+
+def merge_sort(arr, left, right):
+    if left >= right:
+        return
+
+    mid = left + (right - left) // 2
+    merge_sort(arr, left, mid)
+    merge_sort(arr, mid + 1, right)
+    merge(arr, left, mid, right)
+
+
+# Driver code
+arr = [38, 27, 43, 10]
+merge_sort(arr, 0, len(arr) - 1)
+print(" ".join(map(str, arr)))
